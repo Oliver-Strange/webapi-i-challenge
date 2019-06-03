3bc6a8f9f27d5594765d1326b27083e98fd2b09c
Oliver-Strange <preston.burton@gmail.com>
2019-06-03 14:18:53 -0700

    set up basic server info, about to work on post

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // implement your API here
+
+const express = require("express");
+const db = require("./data/db");
+
+const server = express();
+
+server.use(express.json());
+
+const port = 3000;
+
+server.get("/", (req, res) => {
+  res.send("Hello World");
+});
