5cdc67d94214046fbafe1bc610380abab63b3eb3
Oliver-Strange <preston.burton@gmail.com>
2019-06-03 14:27:09 -0700

    forgot to install express, forgot server.listen

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ const port = 3000;
 server.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+server.listen(port, () => {
+  console.log(`Server is Running on http://localhost:${port}`);
+});
