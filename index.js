6c43f1f3081987b1b01a6cca9b2e95f18919f61c
Oliver-Strange <preston.burton@gmail.com>
2019-06-03 15:06:30 -0700

    working on post, get users works

--- index.js
+++ index.js
@@ -9,9 +9,52 @@ server.use(express.json());
 
 const port = 3000;
 
+// GET
 server.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+server.get("/users", (req, res) => {
+  db.find(id)
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "The users information could not be retrieved."
+      });
+    });
+});
+
+// server.get('/users/:id', (req, res) => {
+//   const { id } = req.params;
+//   db.find()
+//     .then()
+// })
+
+// POST
+server.post("/users", (req, res) => {
+  const body = req.body;
+  const { name, bio } = req.body;
+  if (!name || !bio) {
+    res.status.apply(400).json({
+      errorMessage: "Please provide name and bio for the user."
+    });
+  }
+  db.add(body)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "There was an error while saving the user to the database"
+      });
+    });
+});
+
+// PUT
+
+// DELETE
 
 server.listen(port, () => {
   console.log(`Server is Running on http://localhost:${port}`);
